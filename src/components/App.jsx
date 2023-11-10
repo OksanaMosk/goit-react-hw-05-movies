1f57547b2eeff1e27b5bb797b54e9e26f329ca22
OksanaMosk <oks-m3@ukr.net>
2023-11-10 23:59:18 +0200

    5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,19 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
-import css from './App.module.css';
+import { Route, Routes } from 'react-router-dom';
+import Details from 'pages/Details/Details';
 import Home from 'pages/Home/Home';
 import Movies from 'pages/Movies/Movies';
-import Details from 'pages/Details/Details';
 
+import Layout from './Layout/Layout';
 export const App = () => {
   return (
-    <div>
-      <header className={css.header}>
-        <NavLink className={css.headerLink} to="/">
-          Home
-        </NavLink>
-        <NavLink className={css.headerLink} to="/movies">
-          Movies
-        </NavLink>
-      </header>
-      <main className={css.main}>
+    <>
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId/*" element={<Details />} />
         </Routes>
-      </main>
-    </div>
+      </Layout>
+    </>
   );
 };
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import css from './Layout.module.css';
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <header className={css.header}>
+        <NavLink className={css.headerLink} to="/">
+          Home
+        </NavLink>
+        <NavLink className={css.headerLink} to="/movies">
+          Movies
+        </NavLink>
+      </header>
+      <main className={css.main}>{children}</main>
+    </div>
+  );
+};
+export default Layout;
